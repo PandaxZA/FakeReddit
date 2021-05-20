a748144595b8d3c7a6f237d1507d50df9e5578bd
Chris Strybis <chris.strybis@gmail.com>
2021-05-20 17:48:43 +0200

    forgot-password

--- fakereddit-server/src/index.ts
+++ fakereddit-server/src/index.ts
@@ -49,7 +49,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res, redis }),
   });
 
   apolloServer.applyMiddleware({ app, cors: false });
--- fakereddit-server/src/resolvers/user.ts
+++ fakereddit-server/src/resolvers/user.ts
@@ -1,5 +1,6 @@
+import { sendMail } from "../utils/sendEmail";
 import { validateRegister } from "./../utils/validateRegister";
-import { COOKIE_NAME } from "./../constants";
+import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from "./../constants";
 import { User } from "./../entities/User";
 import { MyContext } from "./../types";
 import {
@@ -13,6 +14,7 @@ import {
 } from "type-graphql";
 import argon2 from "argon2";
 import { UsernamePasswordInput } from "./UsernamePasswordInput";
+import { v4 } from "uuid";
 
 @ObjectType()
 class FieldError {
@@ -33,9 +35,29 @@ class UserResponse {
 @Resolver()
 export class UserResolver {
   @Mutation(() => Boolean)
-  async forgotPassword() {
-    // @Ctx() { req, em }: MyContext // @Arg("email") email: string,
-    // const user = await em.findOne(User, { email });
+  async forgotPassword(
+    @Ctx() { em, redis }: MyContext,
+    @Arg("email") email: string
+  ) {
+    const user = await em.findOne(User, { email });
+    if (!user) {
+      return true;
+    }
+
+    const token = v4();
+
+    await redis.set(
+      FORGET_PASSWORD_PREFIX + token,
+      user.id,
+      "ex",
+      1000 * 60 * 60 * 24
+    ); // one day
+
+    await sendMail(
+      email,
+      "Password Reset",
+      `<a href="http://localhost:3000/password-reset/${token}">Reset Password</a>`
+    );
     return true;
   }
   @Query(() => User, { nullable: true })

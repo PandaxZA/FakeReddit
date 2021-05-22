821ad6af1d28e1c9fafb8823be0a13a41243bb65
Chris Strybis <chris.strybis@gmail.com>
2021-05-22 12:42:52 +0200

    windows

--- fakereddit-server/src/mikro-orm.config.ts
+++ fakereddit-server/src/mikro-orm.config.ts
@@ -1,19 +1,21 @@
-import { User } from './entities/User';
-import { Options } from '@mikro-orm/core';
-import { __prod__ } from './constants';
-import { Post } from './entities/Post';
-import path from 'path';
+import { User } from "./entities/User";
+import { Options } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import path from "path";
 
 const config: Options = {
   migrations: {
-    path: path.join(__dirname, './migrations'), // path to the folder with migrations with directory
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations with directory
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
   },
   entities: [Post, User],
-  dbName: 'fakereddit',
-  type: 'postgresql',
+  dbName: "fakereddit",
+  host: "172.22.144.1",
+  port: 5432,
+  type: "postgresql",
   debug: !__prod__,
-  user: 'chrisstrybis',
-  password: 'Byakuya12',
+  user: "postgres",
+  password: "postgres",
 };
 export default config;

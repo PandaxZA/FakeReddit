8eb9505987ecf5f4e28ee4f41f55d25afaec61e9
Chris Strybis <chris.strybis@gmail.com>
2021-06-07 22:45:23 +0200

    pagination done

--- fakereddit-web/src/utils/createUrqlClient.ts
+++ fakereddit-web/src/utils/createUrqlClient.ts
@@ -34,20 +34,33 @@ const cursorPagination = (): Resolver => {
       return undefined;
     }
     const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
-    const isItInTheCache = cache.resolveFieldByKey(entityKey, fieldKey);
+    const isItInTheCache = cache.resolve(
+      cache.resolveFieldByKey(entityKey, fieldKey) as string,
+      "posts"
+    );
 
     info.partial = !isItInTheCache;
 
     const results: string[] = [];
+    let hasMore = true;
     fieldInfos.forEach((fieldInfo) => {
-      const data = cache.resolveFieldByKey(
+      const key = cache.resolveFieldByKey(
         entityKey,
         fieldInfo.fieldKey
-      ) as string[];
+      ) as string;
+      const data = cache.resolve(key, "posts") as string[];
+      const _hasMore = cache.resolve(key, "hasMore");
+      if (!_hasMore) {
+        hasMore = _hasMore as boolean;
+      }
       results.push(...data);
     });
 
-    return results;
+    return {
+      __typename: "PaginatedPosts",
+      hasMore,
+      posts: results,
+    };
 
     // const visited = new Set();
     // let result: NullArray<string> = [];
@@ -111,6 +124,9 @@ export const createUrqlClient = (ssrExchange: any) => ({
   exchanges: [
     dedupExchange,
     cacheExchange({
+      keys: {
+        PaginatedPosts: () => null,
+      },
       resolvers: {
         Query: {
           posts: cursorPagination(),
